5da377b11e2254daeb1caf0d0f34e7c616ee2a43
DESKTOP-13RQSSJ\HP <Ezechukwuchirimmadu@gmail.com>
2023-02-11 20:00:14 +0100

    one

---
+++ old/app.js
@@ -0,0 +1,27 @@
+// alert('holla')
+//data types
+// string
+// int
+//  float
+//  boolean
+
+
+//  let, const
+let a = 'james'
+a = 'j'
+
+let b = []
+b = ['a', 1, 2, 'b']
+
+console.log(b.pop())
+console.log(b)
+
+
+
+
+
+
+
+// console.log(human.name)
+
+
